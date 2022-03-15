adaa03b957a4d711f21393903224b4e433f976a8
WindWaker151 <hinez@mymail.shawnee.edu>
2022-03-15 17:10:10 -0400

    Added the while-loop for the project and some basic necessities. AND the key presses so far.

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 # Judo Space Main
 # March 15, 2022
-# Dustin, Dylan, and Zach
-
-
+# Dustin S, Dylan G, and Zachary H
 
 import pygame
 
@@ -12,7 +10,6 @@ SCREEN_HEIGHT = 600
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Dylan, you can change if you want.
 finished = False
-
 
 class Projectile:
     def __init__(self, start_x, start_y):
@@ -27,24 +24,41 @@ class Projectile:
 
 import pygame
 pygame.init()
-screen = pygame.display.set_mode((480, 640)) # Dylan, you can change if you want.
-finished = False
 clock = pygame.time.Clock()
 
-player_x = 240
-player_y = 600
+player_x = SCREEN_WIDTH / 2
+player_y = (SCREEN_HEIGHT / 2) + (SCREEN_HEIGHT / 3)
 
 while not finished:
     #Update
     delta_time = clock.tick() / 1000
+    player_x = player_x
+    player_y = player_y
 
     # Input
     event = pygame.event.poll()
     if event.type == pygame.quit:
         done = True
-    all_keys = pygame.key.get_pressed()
+    all_keys = pygame.key.get_pressed()     #This is the key inputs
     if all_keys[pygame.K_ESCAPE]:
         finished = True
+
+    if all_keys[pygame.K_a]:     #Left
+        player_x -= 100 * delta_time
+    if all_keys[pygame.K_d]:     #Right
+        player_x += 100 * delta_time
+    if all_keys[pygame.K_w]:     #Up
+        player_y -= 100 * delta_time
+    if all_keys[pygame.K_s]:     #Down
+        player_y += 100 * delta_time
+    if all_keys[pygame.K_e]:     #Dash
+        player_y -= 200 * delta_time
+
+    #Dustin can insert what's needed for imputing the bullet commands
+    all_mouse_buttons = pygame.mouse.get_pressed()
+    #ZDH TO-DO: Find the code for left mouse click
+    if all_keys[pygame.K_SPACE]:    #Fire Bullet
+        pass
 
     # Drawling
     screen.fill((0, 0, 0))
