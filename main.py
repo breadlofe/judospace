1814ce3159646a6761256af643dd8f776acbe752
breadlofe <breadlofe@gmail.com>
2022-03-15 16:46:00 -0400

    Start of the projectile class.

--- main.py
+++ main.py
@@ -7,4 +7,15 @@ pygame.init()
 screen = pygame.display.set_mode((480, 640)) # Dylan, you can change if you want.
 finished = False
 
+
+class Projectile:
+    def __init__(self, start_x, start_y):
+        self.position = [start_x, start_y]
+        self.bullet_list = []
+
+    def update(self, dt):
+        pass
+
+    def draw(self, x, y):
+        pass
 #while not finished:
