fb94ba11286b9627172347abd239c7879892eedb
WindWaker151 <hinez@mymail.shawnee.edu>
2022-03-15 16:42:30 -0400

    Added the while-loop for the project and some basic necessities.

--- main.py
+++ main.py
@@ -6,5 +6,26 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((480, 640)) # Dylan, you can change if you want.
 finished = False
+clock = pygame.time.Clock()
 
-#while not finished:
+player_x = 240
+player_y = 600
+
+while not finished:
+    #Update
+    delta_time = clock.tick() / 1000
+
+    # Input
+    event = pygame.event.poll()
+    if event.type == pygame.quit:
+        done = True
+    all_keys = pygame.key.get_pressed()
+    if all_keys[pygame.K_ESCAPE]:
+        finished = True
+
+    # Drawling
+    screen.fill((0, 0, 0))
+
+    pygame.display.flip()
+
+pygame.display.quit()
