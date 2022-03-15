a4d12e866c5aa6a99ca6db50dc864297ae8542c5
WindWaker151 <hinez@mymail.shawnee.edu>
2022-03-15 16:50:39 -0400

    Added the while-loop for the project and some basic necessities.

--- main.py
+++ main.py
@@ -14,5 +14,41 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Dylan, you can
 finished = False
 
 
+class Projectile:
+    def __init__(self, start_x, start_y):
+        self.position = [start_x, start_y]
+        self.bullet_list = []
 
-#while not finished:
+    def update(self, dt):
+        pass
+
+    def draw(self, x, y):
+        pass
+
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((480, 640)) # Dylan, you can change if you want.
+finished = False
+clock = pygame.time.Clock()
+
+player_x = 240
+player_y = 600
+
+while not finished:
+    #Update
+    delta_time = clock.tick() / 1000
+
+    # Input
+    event = pygame.event.poll()
+    if event.type == pygame.quit:
+        done = True
+    all_keys = pygame.key.get_pressed()
+    if all_keys[pygame.K_ESCAPE]:
+        finished = True
+
+    # Drawling
+    screen.fill((0, 0, 0))
+
+    pygame.display.flip()
+
+pygame.display.quit()
