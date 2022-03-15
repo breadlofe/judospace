b038c30451451442f0de57fea949b037d4d916a5
breadlofe <breadlofe@gmail.com>
2022-03-15 16:33:17 -0400

    Beginning code to get the ball rolling.

--- main.py
+++ main.py
@@ -2,3 +2,9 @@
 # March 15, 2022
 # Dustin, Dylan, and Zach
 
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((480, 640)) # Dylan, you can change if you want.
+finished = False
+
+#while not finished:
