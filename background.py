56ece10d2ea52d5780a0ce748ec8e3c28b58690e
Dylan G <gonzalezd@mymail.shawnee.edu>
2022-03-15 17:08:30 -0400

    Attempted to add a way for stars to self delete

--- background.py
+++ background.py
@@ -9,9 +9,12 @@ class Star :
         self.speed = 200    # Pixels per Second
         self.color = (255, 255, 0)
         self.radius = 1
+        S = Space
 
     def update(self, dt, screen_w, screen_h):
         self.y += self.speed * dt
+        if self.y >= screen_h:
+            S.star_list.remove(0)
 
     def draw(self, surf):
         pygame.draw.circle(surf, self.color, (self.x, self.y), self.radius)
@@ -29,9 +32,12 @@ class Space :
             i += 1
 
         self.add_rate += 1
-        if self.add_rate >= 100
+        if self.add_rate >= 100:
             new_star = Star(screen_w)
             self.star_list.append(new_star)
+            self.add_rate = 0
+
+
 
 
     def draw(self, surf):
