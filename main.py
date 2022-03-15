ee6ea85870c5a23441b4debfa25a2a5c2759eca3
Dylan G <gonzalezd@mymail.shawnee.edu>
2022-03-15 16:45:45 -0400

    Started work on the star and space class

--- main.py
+++ main.py
@@ -2,9 +2,15 @@
 # March 15, 2022
 # Dustin, Dylan, and Zach
 
+
+
 import pygame
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
 pygame.init()
-screen = pygame.display.set_mode((480, 640)) # Dylan, you can change if you want.
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Dylan, you can change if you want.
 finished = False
 
 #while not finished:
