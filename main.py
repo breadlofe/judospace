809024c5d10f9683cdafb1964113108c7a2f811f
breadlofe <98194084+breadlofe@users.noreply.github.com>
2022-03-17 14:53:22 -0400

    CLEANED MAIN PLEASE UPDATE!

--- main.py
+++ main.py
@@ -11,19 +11,6 @@ pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Dylan, you can change if you want.
 finished = False
 
-class Projectile:
-    def __init__(self, start_x, start_y):
-        self.position = [start_x, start_y]
-        self.bullet_list = []
-
-    def update(self, dt):
-        pass
-
-    def draw(self, x, y):
-        pass
-
-import pygame
-pygame.init()
 clock = pygame.time.Clock()
 
 player_x = SCREEN_WIDTH / 2
