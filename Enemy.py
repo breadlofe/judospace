4903aeba0ffc4e35cc45f4ff744c22dd4b2270fe
Dylan G <gonzalezd@mymail.shawnee.edu>
2022-03-17 16:08:28 -0400

    Made the speed 300 Added the enemy file (Incomplete)

---
+++ Enemy.py
@@ -0,0 +1,18 @@
+import pygame
+import random
+
+class Basic_Enemy:
+
+    def __init__(self, radius, start_x):
+        self.radius = radius
+
+        self.start_x = start_x
+        self.start_y = radius * -1
+
+
+    def update(self, dodge):
+
+
+        if dodge == True
+
+        if dodge == False
