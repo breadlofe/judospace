0ae0362d7be207089ae435d75d39b6018b57d5d7
Jason Witherell <jwitherell@shawnee.edu>
2022-03-17 16:38:26 -0400

    modified the order we update bullets

--- projectile.py
+++ projectile.py
@@ -12,13 +12,14 @@ class Projectile:
         self.bullet_list.append([position, lifespan])
 
     def update(self, dt, surf, color):
-        for i in range(len(self.bullet_list) - 1, -1, -1):
-            print(self.bullet_list[i][0][1])
-            self.bullet_list[i][0][1] = self.bullet_list[i][0][1] - 50 * dt
-            self.bullet_list[i][1] = self.bullet_list[i][1] - (1 * dt)
-            if self.bullet_list[i][1] <= 0:
+        for b in self.bullet_list:
+            print(b[0][1])
+            b[0][1] = b[0][1] - 50 * dt
+            b[1] = b[1] - (1 * dt)
+            if b[1] <= 0:
                 print("not")
-                self.bullet_list.pop(i)
+                #self.bullet_list.pop(-i)
+                self.bullet_list.remove(b)
                 print("is")
 
 
