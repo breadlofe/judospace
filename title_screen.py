393bea9dd3ac1325fc9a48eaae73064d2107b9fb
WindWaker151 <hinez@mymail.shawnee.edu>
2022-03-19 18:45:21 -0400

    Added the while-loop for the project and some basic necessities. AND the key presses so far.

--- title_screen.py
+++ title_screen.py
@@ -1,16 +1,24 @@
 import pygame
 
 class Title_Screen:
-    def __init__(self, screen_width, screen_height):   # ,font?
+    def __init__(self, screen_width, screen_height):
+        #Title screen
         self.title_x = screen_width / 2
         self.title_y = screen_height / 3
         self.font = pygame.font.SysFont("Courier New", 20)  #If you want to, you can change the font.
         self.text = "Judo Space"
         self.title_title = self.font.render(str(self.text), False, (250, 250, 0))
 
+        #Play button
         self.button_x = screen_width / 2
         self.button_y = screen_width / 2
         self.button_radius = 32
+
+        #Exit button
+
+        #Credits button
+
+        #Level One
 
     def draw(self, screen):
         screen.blit(self.title_title, (self.title_x, self.title_y))
