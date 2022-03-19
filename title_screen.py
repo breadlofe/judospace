756ac1fac7f52aa5781cb0b97834790a993e1b3b
WindWaker151 <hinez@mymail.shawnee.edu>
2022-03-19 19:22:21 -0400

    Added the while-loop for the project and some basic necessities. AND the key presses so far.

--- title_screen.py
+++ title_screen.py
@@ -6,21 +6,28 @@ class Title_Screen:
         self.title_x = screen_width / 2
         self.title_y = screen_height / 3
         self.font = pygame.font.SysFont("Courier New", 20)  #If you want to, you can change the font.
-        self.text = "Judo Space"
-        self.title_title = self.font.render(str(self.text), False, (250, 250, 0))
-
-        #Play button
-        self.button_x = screen_width / 2
-        self.button_y = screen_width / 2
+        self.title_text = "Judo Space"
+        self.title_title = self.font.render(str(self.title_text), False, (250, 250, 0))
         self.button_radius = 32
 
+        #Play button
+        self.playbutton_x = screen_width / 2
+        self.playbutton_y = screen_width / 2
+
         #Exit button
+        self.playbutton_x = screen_width / 2
+        self.playbutton_y = screen_width / 2
 
         #Credits button
+        self.playbutton_x = screen_width / 2
+        self.playbutton_y = screen_width / 2
 
         #Level One
+        self.one_text = "Level One"
+        self.level_one_display = self.font.render(str(self.one_text), False, (250, 250, 0))
 
     def draw(self, screen):
         screen.blit(self.title_title, (self.title_x, self.title_y))
-        pygame.draw.circle(screen, (255, 120, 0), (self.button_x, self.button_y), self.button_radius)
+        pygame.draw.circle(screen, (255, 120, 0), (self.playbutton_x - (self.button_radius / 2),
+                                                   self.playbutton_y), self.button_radius)
         #Would we need a boolean for the title screen blitting? YES.
