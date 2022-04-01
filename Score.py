05bebc13e63f47e571ac902b985fe8cf6ec88711
WindWaker151 <hinez@mymail.shawnee.edu>
2022-04-01 19:06:17 -0400

    Now added the score class and some additional text to the title screen class.

--- Score.py
+++ Score.py
@@ -8,22 +8,32 @@ class Score:
         self.added_score = score
         self.score = 0
         self.score += self.added_score
+        self.perma_score = 0
 
         # Text
         self.score_score = "Score: " + str(self.score)
         self.score_text = self.font.render(str(self.score_score), False, (255, 255, 0))
         self.score_w = 15
         self.score_h = self.score_text.get_height() / 2
+        self.high = "High Score: " + str(self.perma_score)
+        self.high_score = self.font.render(str(self.high), False, (255, 255, 0))
 
     def add_to_score(self, additive):
         self.added_score = additive
         self.score += self.added_score
 
-        # Text
+        #Text
         self.score_score = "Score: " + str(self.score)
         self.score_text = self.font.render(str(self.score_score), False, (255, 255, 0))
         self.score_w = 15
         self.score_h = self.score_text.get_height() / 2
 
+    def perminate_score(self, new_score):
+        if new_score > self.perma_score:
+            self.perma_score = new_score
+            self.high = "High Score: " + str(self.perma_score)
+            self.high_score = self.font.render(str(self.perma_score), False, (255, 255, 0))
+
     def display_score(self, screen):
         screen.blit(self.score_text, (self.score_w, self.score_h))
+        screen.blit(self.high_score, (self.score_w, self.score_h * 3))
